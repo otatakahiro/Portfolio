aa471a856a132a9353b82211bad8050d9e61ac2e
otatakahiro <ota7281@gmail.com>
2022-12-17 17:09:07 -0800

    dec 17

--- src.js
+++ src.js
@@ -2,6 +2,10 @@ let ac = $('input');
 let a = $('a');
 let aboutPage = ac[1];
 
+$('.logo').on('click', function(){
+  ac[0].click();
+})
+
 let home = function(){
   ac[0].click();
 };
@@ -18,24 +22,25 @@ $('button').on('click',function(){
   ac[1].click();
 });
 
-// $('#ac-cap2').on('click', function(){
-//   const ctx = document.getElementById('myChart');
 
-// //この2つの値を設定するだけ!!
-// const percentage = 76 //グラフのパーセンテージ(76%)
-// const backgroundColor = 'rgba(255, 0, 0, 1)' //グラフの色(赤)
+$('#ac-cap2').on('click', function(){
+  const ctx = document.getElementById('myChart');
 
-// new Chart(ctx, {
-//   type: 'doughnut',
-//   data: {
-//     datasets: [{
-//       data: [percentage, 100 - percentage],
-//       backgroundColor: [
-//         backgroundColor,
-//         'rgba(0, 0, 0, 0)',
-//       ],
-//       borderWidth: 0
-//     }]
-//   }
-// })
-// })
+//この2つの値を設定するだけ!!
+const percentage = 76 //グラフのパーセンテージ(76%)
+const backgroundColor = 'rgba(255, 0, 0, 1)' //グラフの色(赤)
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      data: [percentage, 100 - percentage],
+      backgroundColor: [
+        backgroundColor,
+        'rgba(0, 0, 0, 0)',
+      ],
+      borderWidth: 0
+    }]
+  }
+})
+})
