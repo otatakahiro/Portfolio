49a74639c263cf7e0dedf3654ec3dc2d363ec217
otatakahiro <ota7281@gmail.com>
2022-12-30 22:30:26 -0800

    dec 30

--- src.js
+++ src.js
@@ -32,116 +32,148 @@ const ctx4 = document.getElementById('myChart4');
 const ctx5 = document.getElementById('myChart5');
 const ctx6 = document.getElementById('myChart6');
 const ctx7 = document.getElementById('myChart7');
-
 //この2つの値を設定するだけ!!
-const percentage = 76 //グラフのパーセンテージ(76%)
-const backgroundColor = 'rgba(255, 0, 0, 1)' //グラフの色(赤)
+// const percentage = 76 //グラフのパーセンテージ(76%)
+// const backgroundColor = 'rgba(255, 0, 0, 1)' //グラフの色(赤)
 //スクロール時に要素をフェードインさせるイベントを設定する
-console.log(FADEIN_ELEM)
 Fade.addEventListener('scroll', () => {
     //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP = FADEIN_ELEM.getBoundingClientRect().top;
     console.log(FADEIN_ELEM_TOP)
     //ブラウザの高さを取得
     const WINDOW_HEIGHT = window.innerHeight;
+    console.log(WINDOW_HEIGHT)
     // console.log(WINDOW_HEIGHT);
     //画面に表示された時にフェードインさせる処理
-    if (WINDOW_HEIGHT > (FADEIN_ELEM_TOP) + 100) {
+    if (WINDOW_HEIGHT > (FADEIN_ELEM_TOP) + 200) {
         FADEIN_ELEM.classList.add('fadein-after');
         new Chart(ctx, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [90, 100 - 90],
               backgroundColor: [
-                backgroundColor,
+                '#E34C26',
                 'rgba(0, 0, 0, 0)',
               ],
-              borderWidth: 0
+              borderWidth: 0,
             }]
+          },
+          options: {
+                tooltips: {
+                enabled: false
+            }
           }
         })
         new Chart(ctx2, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [85, 100 - 85],
               backgroundColor: [
-                backgroundColor,
+                '#264DE4',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
-          }
+          },
+          options: {
+            tooltips: {
+            enabled: false
+        }
+      }
         })
         new Chart(ctx3, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [68, 100 - 68],
               backgroundColor: [
-                backgroundColor,
+                '#787CB5',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
-          }
+          },
+          options: {
+            tooltips: {
+            enabled: false
+        }
+      }
         })
         new Chart(ctx4, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [65, 100 - 65],
               backgroundColor: [
-                backgroundColor,
+                '#61DBFB',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
-          }
+          },
+          options: {
+            tooltips: {
+            enabled: false
+        }
+      }
         })
         new Chart(ctx5, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [75, 100 - 75],
               backgroundColor: [
-                backgroundColor,
+                '#F0DB4F',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
-          }
+          },
+          options: {
+            tooltips: {
+            enabled: false
+        }
+      }
         })
         new Chart(ctx6, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [82, 100 - 82],
               backgroundColor: [
-                backgroundColor,
+                '#DD1D16',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
-          }
+          },
+          options: {
+            tooltips: {
+            enabled: false
+        }
+      }
         })
         new Chart(ctx7, {
           type: 'doughnut',
           data: {
             datasets: [{
-              data: [percentage, 100 - percentage],
+              data: [80, 100 - 80],
               backgroundColor: [
-                backgroundColor,
+                '#41B883',
                 'rgba(0, 0, 0, 0)',
               ],
               borderWidth: 0
             }]
+          },
+          options: {
+                tooltips: {
+                enabled: false
+            }
           }
         })
     }else{
         FADEIN_ELEM.classList.remove('fadein-after');
     }
-
 });
-
